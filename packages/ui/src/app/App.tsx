8244f8989c8fc7ac40b74fc6e210afabf35be848
Alex Gherghisan <alexghr@users.noreply.github.com>
2020-01-21 22:33:36 +0000

    Scaffold country selector

--- packages/ui/src/app/App.tsx
+++ packages/ui/src/app/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import Select from './Select';
+import { countries } from './countries';
 
 const Element = styled.div`
   background-color: red;
 `;
 
 const App: React.FC = () => {
-  return <Element>this is the app root</Element>
+  const [country, selectCountry] = React.useState(countries[0].value);
+
+  React.useEffect(() => {
+    console.log('selected country %s', country);
+  }, [country]);
+
+  return (
+    <Element>
+      <Select options={countries} selected={country} onChange={selectCountry} />
+    </Element>
+  );
 }
 
 export default App;
---
+++ packages/ui/src/app/countries.ts
@@ -0,0 +1,22 @@
+export const countries = [
+  {
+    value: 'gb',
+    label: 'United Kingdom'
+  },
+  {
+    value: 'us',
+    label: 'United States'
+  },
+  {
+    value: 'fr',
+    label: 'France'
+  },
+  {
+    value: 'au',
+    label: 'Australia'
+  },
+  {
+    value: 'in',
+    label: 'India'
+  }
+];
