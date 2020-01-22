8bbb27f2df6db37b137533aea4c30961cdabbde5
Alex Gherghisan <alexghr@users.noreply.github.com>
2020-01-22 21:52:00 +0000

    Add top-level error handler middleware

--- packages/server/src/lib/server.ts
+++ packages/server/src/lib/server.ts
@@ -7,6 +7,16 @@ import api from './api';
 export default async function createServer() {
   const server = new Koa();
 
+  server.use(async (ktx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      console.error("Error processing request %s %s: %o", ktx.method, ktx.path, err);
+      ktx.status = 500;
+      ktx.body = 'Internal server error';
+    }
+  });
+
   server.use(api());
 
   server.use(serveStatic(staticDir));
