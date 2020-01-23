b9843c6f23e5ea8cdc2c015aff7c3b33cb8d766e
Alex Gherghisan <alexghr@users.noreply.github.com>
2020-01-23 23:59:23 +0000

    Add grid of articles

--- packages/ui/src/app/components/TopHeadlines.tsx
+++ packages/ui/src/app/components/TopHeadlines.tsx
@@ -3,6 +3,8 @@ import { SelectedCountryContext } from '../context/SelectedCountryContext';
 import useHeadlines from '../hooks/useHeadlines';
 import styled from 'styled-components';
 import LoadingIndicator from './LoadingIndicator';
+import { mediaQuery, spacing } from '../const';
+import { Container } from './Container';
 
 const TopHeadlines: React.FC = () => {
   const { country } = React.useContext(SelectedCountryContext);
@@ -11,7 +13,19 @@ const TopHeadlines: React.FC = () => {
   return (
     <Element>
       {loading && <LoadingIndicator />}
-      {data && data.map(({ title }) => <Article>{title}</Article>)}
+      {data && (
+        <ArticleContainer>
+          {data.map(({ url, urlToImage, title, description }) => (
+            <ExternalLink href={url} key={url}>
+              <Article>
+                <Img src={urlToImage} />
+                <Title>{title}</Title>
+                <Description>{description}</Description>
+              </Article>
+            </ExternalLink>
+          ))}
+        </ArticleContainer>
+      )}
       {error && <>There was an error loading your news. Try refreshing this page</>}
     </Element>
   );
@@ -19,8 +33,63 @@ const TopHeadlines: React.FC = () => {
 
 export default TopHeadlines;
 
-const Element = styled.div`
-  padding: 16px 0;
+const Element = styled(Container)`
+  margin: ${spacing.large} auto;
+
+  @media ${mediaQuery.tablet} {
+    margin: ${spacing.base};
+  }
+
+  @media ${mediaQuery.phone} {
+    margin: ${spacing.small};
+  }
 `;
 
-const Article = styled.div``;
+const ArticleContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: ${spacing.large};
+
+  @media ${mediaQuery.tablet} {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: ${spacing.base}
+  }
+
+  @media ${mediaQuery.phone} {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: ${spacing.small};
+  }
+`;
+
+const ExternalLink = styled.a`
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+const Article = styled.article`
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: contain;
+  background-color: black;
+
+  @media ${mediaQuery.tablet} {
+    height: 150px;
+  }
+
+  @media ${mediaQuery.phone} {
+    height: 100px;
+  }
+`;
+
+const Title = styled.span`
+  display: block;
+  font-weight: bold;
+  text-decoration: underline;
+`;
+
+const Description = styled.p`
+`;
